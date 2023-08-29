cb7246d7784ae1f93f700a2c655812633f7f92fb
kaminenisagar <kaminenisagar@gmail.com>
2023-08-29 14:52:04 +0000

    solution

---
+++ src/components/ClickCounter/index.js
@@ -0,0 +1,13 @@
+import {component} from 'react'
+import './index.css'
+
+class ClickCounter extends component {
+  render() {
+    return (
+        <div className="click-container">
+            
+        </div>
+    )
+  }
+}
+export default ClickCounter
